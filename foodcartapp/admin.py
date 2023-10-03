b5ab6d52db215c9021c7c57e39ce7c105deb62a1
SGK <55636018+SGKespace@users.noreply.github.com>
2023-10-03 19:48:59 +0400

    Добавлено перенаправление для менеджеров

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.http import HttpResponseRedirect
 
-
-from .models import Order
-from .models import OrderItem
 from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
 from .models import RestaurantMenuItem
+from .models import Order
+from .models import OrderItem
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -119,6 +120,13 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline
     ]
+
+    def response_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET and url_has_allowed_host_and_scheme(request.GET['next'], None):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
 
 
 @admin.register(OrderItem)
