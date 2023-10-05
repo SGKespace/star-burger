871da54d80dea1a19f254f7eab348cb40884a911
SGK <55636018+SGKespace@users.noreply.github.com>
2023-10-05 14:55:30 +0400

    Добавлена ​​база геоданных.

---
+++ geo_data/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class GeoData(models.Model):
+    address = models.CharField(
+        'адрес',
+        max_length=100,
+        blank=True,
+        unique=True,
+        db_index=True,
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+    )
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+    )
+    created_at = models.DateTimeField(
+        default=now,
+    )
