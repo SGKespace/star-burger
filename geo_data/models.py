c2421bceb417553008081b410b64acbeda8c9c82
SGK <55636018+SGKespace@users.noreply.github.com>
2023-10-19 16:22:20 +0400

    Вынес запрос к геокодеру в отдельную функцию

--- geo_data/models.py
+++ geo_data/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.db import models
 from django.utils.timezone import now
 
+from geo_data import geo_lacation
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'star_burger.settings'
 
@@ -36,26 +38,4 @@ class GeoData(models.Model):
         try:
             return GeoData.objects.get(address=address)
         except GeoData.DoesNotExist:
-            base_url = "https://geocode-maps.yandex.ru/1.x"
-            response = requests.get(
-                base_url,
-                params={
-                    'geocode': address,
-                    'apikey': apikey,
-                    'format': "json",
-                },
-                timeout=10,
-            )
-            response.raise_for_status()
-            found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-            if not found_places:
-                return None
-
-            most_relevant = found_places[0]
-            lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-            return GeoData.objects.create(
-                address=address,
-                latitude=lat,
-                longitude=lon,
-            )
+            return geo_lacation.get_or_create_locations(address, apikey=settings.YANDEX_API_KEY)
