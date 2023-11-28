2f2dee6a0bab8b53cfbcdf617ce88aac85743b26
SGKespace <sgkond@gmail.com>
2023-11-28 07:20:10 +0000

    рабочий вариант перед rollbar

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,8 +15,9 @@ SECRET_KEY = env('SECRET_KEY')
 YANDEX_API_KEY = env('YANDEX_API_KEY')
 DEBUG = env.bool('DEBUG', False)
 
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
-
+# ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
     'restaurateur.apps.RestaurateurConfig',
--- star_burger/wsgi.py
+++ star_burger/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
+# sys.path.append('/opt/star-burger/star_burger')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "star_burger.settings")
 application = get_wsgi_application()
